e1893b7807b10960022d82f747e4089c2026d327
Sourav Bishai <106469372+techCodeSB@users.noreply.github.com>
2026-04-17 20:19:01 +0530

    add production url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constant {
   // static String apiUrl = "http://10.0.2.2:8000";
-  static String apiUrl = "http://10.235.167.231:8000/api/v1";
+  static String apiUrl = "https://parenting-community-wall-server.onrender.com/api/v1";
 }
